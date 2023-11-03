a178c98b6b79cecab5f8df10ee9f016d3dd788bb
Rob Bridges <Rmb101187@gmail.com>
2023-11-03 16:01:30 -0700

    3 sum fix

--- moveDuplicates.go
+++ moveDuplicates.go
@@ -1,14 +1,13 @@
 package main
 
 func removeDuplicates(nums []int) int {
-	prev := nums[0]
-	   l := 1
-	   for i := 1; i < len(nums); i++ {
-		   if nums[i] != prev {
-			   nums[l] = nums[i]
-			   l++
-		   } 
-		   prev = nums[i]
-	   }
-	   return l
+	prev := 0
+	l := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[prev] {
+			nums[l] = nums[prev]
+			l++
+		}
+	}
+	return l
    }
