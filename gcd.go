e4dcedd0c5e99015e9b9163353be94c2de7973bf
Rob Bridges <Rmb101187@gmail.com>
2023-11-13 08:48:26 -0800

    GCD of strings

---
+++ gcd.go
@@ -0,0 +1,20 @@
+package main
+
+func gcdOfStrings(str1 string, str2 string) string {
+    
+    if str1+str2 != str2 + str1 {
+        return ""
+    }
+
+    len := gcd( len(str1), len(str2) )
+    return str1[:len]
+}
+
+func gcd( a int, b int ) int {
+
+    for b != 0 {
+        a , b = b , a%b
+    }
+
+    return a
+}
