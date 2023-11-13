47e5e449d74602ec37930b53f9bb5a7d5988885e
Rob Bridges <Rmb101187@gmail.com>
2023-11-12 22:02:22 -0800

    merge sorted array

--- mergeSortedArray.go
+++ mergeSortedArray.go
@@ -3,25 +3,23 @@ package main
 
 
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    p1 := m - 1
-    p2 := n - 1
-    p := m + n - 1
+    insertPoint := m + n - 1
 
-    for p1 >= 0 && p2 >= 0 {
-        if nums1[p1] > nums2[p2] {
-            nums1[p] = nums1[p1]
-            p1--
+    for m > 0 && n > 0 {
+        if nums1[m - 1] > nums2[n - 1] {
+            nums1[insertPoint] = nums1[m - 1]
+           m--
         } else {
-            nums1[p] = nums2[p2]
-            p2--
+            nums1[insertPoint] = nums2[n - 1]
+            n--
         }
-        p--
+        insertPoint--
     }
 
     // add missing elements from nums2
-    for p2 >= 0 {
-        nums1[p] = nums2[p2]
-        p2--
-        p--
+    for n > 0 {
+        nums1[insertPoint] = nums2[n - 1]
+        n--
+        insertPoint--
     }
 }
