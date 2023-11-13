9a99974fceb93b9a1b02df6f432eb5b1e2d3e082
Rob Bridges <Rmb101187@gmail.com>
2023-11-12 22:52:11 -0800

    Updated merge sorted array based on assumptions

--- mergeSortedArray.go
+++ mergeSortedArray.go
@@ -22,4 +22,10 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
         n--
         insertPoint--
     }
+
+    for m > 0 {
+        nums1[insertPoint] = nums1[m - 1] 
+        m--
+        insertPoint--
+    }
 }
