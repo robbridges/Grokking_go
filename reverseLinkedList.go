bc74b9878b9c2cb1da2869d245d3581fd91ab348
Rob Bridges <Rmb101187@gmail.com>
2023-11-21 17:40:49 -0800

    Found a more concise way to reverse linked list

--- reverseLinkedList.go
+++ reverseLinkedList.go
@@ -6,22 +6,15 @@ type ListNode struct {
 }
 
 func ReverseList(head *ListNode) *ListNode {
-	// return nil if the list is empty
-    if head == nil {
-        return nil
-    }
-    
-    var revHead *ListNode
-	// while head exists
-    for head != nil {
-		// temp variable that we need
-        tmp := head.Next
-		// move head forward, we still have acopy of it in temp
-        head.Next = revHead
-		// set revhead to the head
-        revHead = head
-		// set head to the temp
-        head = tmp
-    }
-    return revHead
+	var previousNode *ListNode = nil
+	currentNode := head
+	
+	for currentNode != nil {
+		nextNode := currentNode.Next
+		currentNode.Next = previousNode
+		previousNode = currentNode
+		currentNode = nextNode
+	}
+	
+	return previousNode
 }
