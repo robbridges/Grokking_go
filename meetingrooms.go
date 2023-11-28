80d256d18dc209536f5ac1449783cfca33a067d0
Rob Bridges <Rmb101187@gmail.com>
2023-11-28 08:30:45 -0800

    More fixes to linting

--- meetingrooms.go
+++ meetingrooms.go
@@ -11,18 +11,18 @@ func canAttendMeetings(intervals [][]int) bool {
 	starts := []int{}
 	ends := []int{}
 	for _, interval := range intervals {
-    starts = append(starts, interval[0])
-    ends = append(ends, interval[1])
-}
-sort.Ints(starts)
-sort.Ints(ends)
+		starts = append(starts, interval[0])
+		ends = append(ends, interval[1])
+	}
+	sort.Ints(starts)
+	sort.Ints(ends)
 
-for i := 0; i < len(intervals) - 1; i++ {
+	for i := 0; i < len(intervals) - 1; i++ {
     // check to see if the next meeting starts before this meeting ends if so, return false
-    if starts[i+1] < ends[i] {
-        return false
-    }
-}
+		if starts[i+1] < ends[i] {
+			return false
+		}
+	}
 
 return true
 }
